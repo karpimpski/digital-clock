89c01586c8d016bd804b0427dd2ea5eb5b4156c2
Michael Karpinski <mike.karpinski99@gmail.com>
2019-02-25 16:03:57 -0500

    added colon to separate hour and minute and began clock script

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 require('../scss/main.scss')
 
-const numbers = document.getElementsByClassName('number')
+const numberElements = document.getElementsByClassName('number')
 
 // instructions for filling each shape
 const shapes = [
@@ -16,23 +16,37 @@ const shapes = [
   [ [0, 0], [1, 0, 1, 2], [2, 0, 1]]
 ]
 
-demo(0)
-
-function demo(i) {
-  setInterval(function() {
-    draw(i, numbers[0])
-    draw(i++, numbers[2])
-    if (i == shapes.length) i = 0
-  }, 500)
+let date = new Date()
+let hour = date.getHours() % 12 ? date.getHours() % 12 : 12
+if (hour > 9) {
+  draw(0, 1)
+  draw(0, hour % 12)
+}
+else {
+  draw(0, 0)
+  draw(1, hour)
+}
+let minute = date.getMinutes()
+if (minute == 0) {
+  draw(2, 0)
+  draw(3, 0)
+}
+else if(minute < 10) {
+  draw(2, 0)
+  draw(3, minute)
+}
+else {
+  draw(2, Math.floor(minute / 10))
+  draw(3, minute % 10)
 }
 
 /**
  * Draws given number by filling each column individually.
  */
 function draw(index, number) {
-  clear(number)
-  shapes[index].forEach(function(column) {
-    fill(number, ...column)
+  clear(numberElements[index])
+  shapes[number].forEach(function(column) {
+    fill(numberElements[index], ...column)
   })
 }
 
@@ -40,6 +54,7 @@ function draw(index, number) {
  * Fills column based on which line indexes are chosen (0 is the top line).
  */
 function fill(number, columnIndex, ...lineIndexes) {
+  console.log(number)
   const column = number.getElementsByClassName('column')[columnIndex]
   lineIndexes.forEach(i => {
     column.getElementsByClassName('line')[i].classList.add('active')
